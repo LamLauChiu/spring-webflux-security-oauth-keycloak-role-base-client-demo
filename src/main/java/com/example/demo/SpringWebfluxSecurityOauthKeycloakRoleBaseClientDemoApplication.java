13c96f1590579f3911a2d8a633cb5e61aff1fdcb
LauChiu Lam <lamlauchiu@gmail.com>
2021-04-27 16:51:42 +0800

    no message

---
+++ src/main/java/com/example/demo/SpringWebfluxSecurityOauthKeycloakRoleBaseClientDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebfluxSecurityOauthKeycloakRoleBaseClientDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebfluxSecurityOauthKeycloakRoleBaseClientDemoApplication.class, args);
+	}
+
+}
